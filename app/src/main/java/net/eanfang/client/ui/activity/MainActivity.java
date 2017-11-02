781851208812df181391d22aec533dfdf79ae8db
houzhongzhou <1030548987@qq.com>
2017-11-02 13:38:23 +0800

    分包

--- app/src/main/java/net/eanfang/client/ui/activity/MainActivity.java
+++ app/src/main/java/net/eanfang/client/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package net.eanfang.client;
+package net.eanfang.client.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import net.eanfang.client.R;
 
 public class MainActivity extends AppCompatActivity {
 
