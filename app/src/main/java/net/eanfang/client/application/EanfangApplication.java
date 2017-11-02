4b0991d2b2371ffe1f4d4ecb0274f87ffa8ec51d
houzhongzhou <1030548987@qq.com>
2017-11-02 13:50:33 +0800

    添加assets包和keystore包

---
+++ app/src/main/java/net/eanfang/client/application/EanfangApplication.java
@@ -0,0 +1,12 @@
+package net.eanfang.client.application;
+
+/**
+ * Created by Mr.hou
+ *
+ * @on 2017/11/2  13:46
+ * @email houzhongzhou@yeah.net
+ * @desc 配置一些sdk
+ */
+
+public class EanfangApplication {
+}
