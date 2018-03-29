940da6edd78e4c049d1a81ce1a6b8b4774a06af9
MrHou <1030548987@qq.com>
2018-03-29 16:43:56 +0800

    泛型改为String

--- eanfang/src/main/java/com/eanfang/localcache/CacheGetCallBack.java
+++ eanfang/src/main/java/com/eanfang/localcache/CacheGetCallBack.java
@@ -4,6 +4,6 @@ package com.eanfang.localcache;
  * Created by xudingbing on 2018/3/29.
  */
 
-public interface CacheGetCallBack<T> {
-    public void readValue(T result);
+public interface CacheGetCallBack {
+    public void readValue(String result);
 }
--- eanfang/src/main/java/com/eanfang/localcache/CacheGetCallBackMulti.java
+++ eanfang/src/main/java/com/eanfang/localcache/CacheGetCallBackMulti.java
@@ -6,6 +6,6 @@ import java.util.List;
  * Created by xudingbing on 2018/3/29.
  */
 
-public interface CacheGetCallBackMulti<T> {
-    public void readValue(List<T> result);
+public interface CacheGetCallBackMulti{
+    public void readValue(List<String> result);
 }
