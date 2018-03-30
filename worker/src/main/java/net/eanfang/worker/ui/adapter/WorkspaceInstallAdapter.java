86caba5797d757d0680dc9f688aeabaccac929af
MrHou <1030548987@qq.com>
2018-03-30 15:10:56 +0800

    修改相关代码

--- worker/src/main/java/net/eanfang/worker/ui/adapter/WorkspaceInstallAdapter.java
+++ worker/src/main/java/net/eanfang/worker/ui/adapter/WorkspaceInstallAdapter.java
@@ -11,6 +11,8 @@ import net.eanfang.worker.R;
 
 import java.util.List;
 
+import static com.eanfang.util.V.v;
+
 /**
  * 工作台-我要报装adapter
  * Created by Administrator on 2017/3/15.
@@ -24,14 +26,14 @@ public class WorkspaceInstallAdapter extends BaseQuickAdapter<WorkspaceInstallBe
     @Override
     protected void convert(BaseViewHolder helper, WorkspaceInstallBean.ListBean item) {
         //报装列表 增加 联系人
-        helper.setText(R.id.tv_company_name, item.getClientCompanyName() + "  (" + item.getConnector() + ")")
-                .setText(R.id.tv_order_id, "单号：" + item.getOrderNo())
+        helper.setText(R.id.tv_company_name, v(() -> item.getClientCompanyName() + "  (" + item.getConnector() + ")"))
+                .setText(R.id.tv_order_id, "单号：" + v(() -> item.getOrderNo()))
                 //下单时间 显示错误 已修改
-                .setText(R.id.tv_order, "下单：" + item.getCreateTime())
-                .setText(R.id.tv_time, "工期：" + Config.get().getConstBean().getData()
-                        .getDesignOrderConstant().get(Constant.PREDICTTIME_TYPE).get(item.getPredictTime()))
-                .setText(R.id.tv_business, "业务：" + item.getBusinessOneCode())
-                .setText(R.id.tv_count_money, GetConstDataUtils.getBudgetList().get(item.getBudget()));
+                .setText(R.id.tv_order, "下单：" + v(() -> item.getCreateTime()))
+                .setText(R.id.tv_time, "工期：" + v(() -> Config.get().getConstBean().getData()
+                        .getDesignOrderConstant().get(Constant.PREDICTTIME_TYPE).get(item.getPredictTime())))
+                .setText(R.id.tv_business, "业务：" + v(() -> Config.get().getBusinessNameByCode(item.getBusinessOneCode(), 1)))
+                .setText(R.id.tv_count_money, v(() -> GetConstDataUtils.getBudgetList().get(item.getBudget())));
         helper.setVisible(R.id.iv_upload, false);
 
     }
