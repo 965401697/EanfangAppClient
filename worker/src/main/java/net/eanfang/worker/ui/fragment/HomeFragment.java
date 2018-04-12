d817888882d3e61fa4b5dc3860c7f8d25db0ed99
guanluocang <guanluocang@163.com>
2018-04-12 17:47:17 +0800

    1、修改技师端布局 2、整合两个端公共部分到eanfang

--- worker/src/main/java/net/eanfang/worker/ui/fragment/HomeFragment.java
+++ worker/src/main/java/net/eanfang/worker/ui/fragment/HomeFragment.java
@@ -2,13 +2,18 @@ package net.eanfang.worker.ui.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.listener.OnItemClickListener;
 import com.eanfang.application.EanfangApplication;
+import com.eanfang.model.ClientData;
 import com.eanfang.ui.base.BaseFragment;
 import com.eanfang.witget.BannerView;
 import com.eanfang.witget.RollTextView;
@@ -19,6 +24,7 @@ import net.eanfang.worker.ui.activity.worksapce.QuotationActivity;
 import net.eanfang.worker.ui.activity.worksapce.TakeTaskListActivity;
 import net.eanfang.worker.ui.activity.worksapce.TaskPublishActivity;
 import net.eanfang.worker.ui.activity.worksapce.WebActivity;
+import net.eanfang.worker.ui.adapter.HomeDataAdapter;
 import net.eanfang.worker.ui.widget.SignCtrlView;
 
 import java.util.ArrayList;
@@ -37,7 +43,10 @@ public class HomeFragment extends BaseFragment {
     private BannerView bannerView;
 
     private RollTextView rollTextView;
-    private LinearLayout ll_assign_packpage, ll_accept_packpage, ll_tools_store, ll_quote_add, ll_sign;
+
+    private RecyclerView rvData;
+    private List<ClientData> clientDataList = new ArrayList<>();
+    private HomeDataAdapter homeDataAdapter;
 
     @Override
     protected int setLayoutResouceId() {
@@ -51,26 +60,38 @@ public class HomeFragment extends BaseFragment {
 
     @Override
     protected void initView() {
+        rvData = (RecyclerView) findViewById(R.id.rv_data);
         initIconClick();
         initLoopView();
         initRollTextView();
+        //设置布局样式
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        rvData.setLayoutManager(linearLayoutManager);
         initCount();
+        initFalseData();
     }
 
     /**
      * 工作按钮
      */
     private void initIconClick() {
-        ll_assign_packpage = findViewById(R.id.ll_assign_packpage);
-        ll_accept_packpage = findViewById(R.id.ll_accept_packpage);
-        ll_tools_store = findViewById(R.id.ll_tools_store);
-        ll_quote_add = findViewById(R.id.ll_quote_add);
-        ll_sign = findViewById(R.id.ll_sign);
-        ll_assign_packpage.setOnClickListener(v -> startActivity(new Intent(getActivity(), TaskPublishActivity.class)));
-        ll_accept_packpage.setOnClickListener(v -> startActivity(new Intent(getActivity(), TakeTaskListActivity.class)));
-        ll_tools_store.setOnClickListener(v -> showToast("本店暂未开业"));
-        ll_quote_add.setOnClickListener(v -> startActivity(new Intent(getActivity(), QuotationActivity.class)));
-        ll_sign.setOnClickListener((v) -> {
+        //报修订单
+        findViewById(R.id.tv_reparir_order).setOnClickListener(v -> startActivity(new Intent(getActivity(), TaskPublishActivity.class)));
+        //报装订单
+        findViewById(R.id.tv_install_order).setOnClickListener(v -> startActivity(new Intent(getActivity(), TaskPublishActivity.class)));
+        //设计订单
+        findViewById(R.id.tv_design_order).setOnClickListener(v -> startActivity(new Intent(getActivity(), TaskPublishActivity.class)));
+        //维保订单
+        findViewById(R.id.tv_maintain_order).setOnClickListener(v -> startActivity(new Intent(getActivity(), TaskPublishActivity.class)));
+        //发包
+        findViewById(R.id.tv_project_send).setOnClickListener(v -> startActivity(new Intent(getActivity(), TaskPublishActivity.class)));
+        //接包
+        findViewById(R.id.tv_project_receive).setOnClickListener(v -> startActivity(new Intent(getActivity(), TakeTaskListActivity.class)));
+        //内部报价
+        findViewById(R.id.tv_inside_price).setOnClickListener(v -> startActivity(new Intent(getActivity(), QuotationActivity.class)));
+        //签到
+        findViewById(R.id.tv_sign).setOnClickListener((v) -> {
             new SignCtrlView(getActivity()).show();
         });
     }
@@ -85,9 +106,12 @@ public class HomeFragment extends BaseFragment {
      * 统计
      */
     private void initCount() {
-        findViewById(R.id.rl_today_repair).setOnClickListener(v -> jumpWebview());
-        findViewById(R.id.rl_free_install).setOnClickListener(v -> jumpWebview());
-        findViewById(R.id.rl_free_design).setOnClickListener(v -> jumpWebview());
+        rvData.addOnItemTouchListener(new OnItemClickListener() {
+            @Override
+            public void onSimpleItemClick(BaseQuickAdapter adapter, View view, int position) {
+                jumpWebview();
+            }
+        });
     }
 
     private void jumpWebview() {
@@ -127,16 +151,24 @@ public class HomeFragment extends BaseFragment {
     private void initRollTextView() {
         rollTextView = findViewById(R.id.home_recommand_ad_text);
         List<String> data = new ArrayList<>();
+        List<String> titleList = new ArrayList<>();
         List<View> views = new ArrayList<>();
         data.add("出席会议的有易安防老大王兴军");
         data.add("出席会议的有易安防总经理吴建华");
         data.add("出席会议的有易安防技术大牛徐定兵");
         data.add("出席会议的有易安防产品经理郭林");
         data.add("出席会议的有没头衔的我，因为太帅，人称帅侯");
+
+        titleList.add("王兴军");
+        titleList.add("吴建华");
+        titleList.add("徐定兵");
+        titleList.add("郭林");
+        titleList.add("帅侯");
         for (int i = 0; i < data.size(); i++) {
             View view = View.inflate(getContext(), R.layout.rolltext_item, null);
             TextView content = (TextView) view.findViewById(R.id.tv_roll_item_text);
             TextView title = (TextView) view.findViewById(R.id.tv_roll_item_title);
+            title.setText(titleList.get(i).toString());
             content.setText(data.get(i).toString());
             views.add(view);
         }
@@ -146,4 +178,45 @@ public class HomeFragment extends BaseFragment {
         });
     }
 
+    private void initFalseData() {
+        ClientData clientDataOne = new ClientData();
+        clientDataOne.setType(1);
+        clientDataOne.setTotal(23);
+        clientDataOne.setAdded(5);
+        clientDataOne.setStatusOne(16);
+        clientDataOne.setStatusTwo(2);
+        clientDataOne.setStatusThree(1);
+        clientDataOne.setStatusFour(14);
+        clientDataList.add(clientDataOne);
+        ClientData clientDataTwo = new ClientData();
+        clientDataTwo.setType(2);
+        clientDataTwo.setTotal(16);
+        clientDataTwo.setAdded(7);
+        clientDataTwo.setStatusOne(18);
+        clientDataTwo.setStatusTwo(9);
+        clientDataTwo.setStatusThree(4);
+        clientDataTwo.setStatusFour(2);
+        clientDataList.add(clientDataTwo);
+        ClientData clientDataThree = new ClientData();
+        clientDataThree.setType(3);
+        clientDataThree.setTotal(37);
+        clientDataThree.setAdded(3);
+        clientDataThree.setStatusOne(12);
+        clientDataThree.setStatusTwo(3);
+        clientDataThree.setStatusThree(6);
+        clientDataThree.setStatusFour(8);
+        clientDataList.add(clientDataThree);
+        ClientData clientDataFour = new ClientData();
+        clientDataFour.setType(4);
+        clientDataFour.setTotal(27);
+        clientDataFour.setAdded(7);
+        clientDataFour.setStatusOne(13);
+        clientDataFour.setStatusTwo(6);
+        clientDataFour.setStatusThree(8);
+        clientDataFour.setStatusFour(0);
+        clientDataList.add(clientDataFour);
+
+        homeDataAdapter = new HomeDataAdapter(clientDataList);
+        rvData.setAdapter(homeDataAdapter);
+    }
 }
