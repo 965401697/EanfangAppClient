c1b035b1dda5e5d1ef699f2a7cf067cdf28b8bae
ziwu <zhaoziwu@yeah.net>
2018-06-08 20:47:22 +0800

    时间选择方法冲突

--- eanfang/src/main/java/com/eanfang/util/ETimeUtils.java
+++ eanfang/src/main/java/com/eanfang/util/ETimeUtils.java
@@ -18,13 +18,13 @@ public class ETimeUtils {
         return format.format(date);
     }
 
-    /**
-     * 获取年月日
-     */
-    public static String getTimeByYearMonthDayHourMinSec(Date date) {//可根据需要自行截取数据显示
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        return format.format(date);
-    }
+//    /**
+//     * 获取年月日
+//     */
+//    public static String getTimeByYearMonthDayHourMinSec(Date date) {//可根据需要自行截取数据显示
+//        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        return format.format(date);
+//    }
     /**
      * 获取年月日
      */
