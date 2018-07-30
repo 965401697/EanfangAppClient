cc659a886843ad102795c630f509cdf747ae61fa
guanluocang <guanluocang@163.com>
2018-07-30 09:10:57 +0800

    1、交接班创建详情页面

--- client/src/main/java/net/eanfang/client/ui/activity/worksapce/worktransfer/WorkTransferCreateDetailActivity.java
+++ client/src/main/java/net/eanfang/client/ui/activity/worksapce/worktransfer/WorkTransferCreateDetailActivity.java
@@ -3,6 +3,8 @@ package net.eanfang.client.ui.activity.worksapce.worktransfer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.eanfang.ui.base.BaseActivity;
+
 import net.eanfang.client.R;
 
 /**
@@ -10,11 +12,17 @@ import net.eanfang.client.R;
  * @date on 2018/7/27  15:45
  * @decision 创建交接班详情
  */
-public class WorkTransferCreateDetailActivity extends AppCompatActivity {
+public class WorkTransferCreateDetailActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_transfer_create_detail);
+        initView();
+    }
+
+    private void initView() {
+        setLeftBack();
+        setTitle("交接班");
     }
 }
