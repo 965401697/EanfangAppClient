acc2297a8547739200222aae240e27e7640a8fdd
jornl <jornlin@foxmail.com>
2018-09-03 11:25:47 +0800

    系统消息格式调整

--- client/src/main/java/net/eanfang/client/ui/activity/worksapce/notice/SystemNoticeDetailActivity.java
+++ client/src/main/java/net/eanfang/client/ui/activity/worksapce/notice/SystemNoticeDetailActivity.java
@@ -71,7 +71,7 @@ public class SystemNoticeDetailActivity extends BaseActivity {
                         if (bean.getExtInfo() != null && !bean.getExtInfo().toString().contains("{")) {
                             extInfo = bean.getExtInfo().toString();
                         }
-                        tvDetailContent.setText(bean.getContent() + (extInfo != null ? extInfo : ""));
+                        tvDetailContent.setText(bean.getContent() + "\r\n\t" + "\r\n\t" + (extInfo != null ? extInfo : ""));
                         tvDetailTime.setText(GetDateUtils.dateToDateTimeString(bean.getCreateTime()));
                     });
                 })));
