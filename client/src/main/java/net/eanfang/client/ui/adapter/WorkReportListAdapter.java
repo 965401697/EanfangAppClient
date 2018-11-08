f97a2918b188fd8ab1273db9993f48ffec3dc3b1
ziwu <zhaoziwu@yeah.net>
2018-11-08 10:24:53 +0800

    客户端工作汇报

--- client/src/main/java/net/eanfang/client/ui/adapter/WorkReportListAdapter.java
+++ client/src/main/java/net/eanfang/client/ui/adapter/WorkReportListAdapter.java
@@ -2,6 +2,7 @@ package net.eanfang.client.ui.adapter;
 
 import android.graphics.Color;
 import android.net.Uri;
+import android.view.View;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
@@ -14,6 +15,7 @@ import com.facebook.drawee.view.SimpleDraweeView;
 
 import net.eanfang.client.R;
 
+
 /**
  * Created by Mr.hou
  *
@@ -23,32 +25,43 @@ import net.eanfang.client.R;
  */
 
 public class WorkReportListAdapter extends BaseQuickAdapter<WorkReportListBean.ListBean, BaseViewHolder> {
-    public WorkReportListAdapter() {
-        super(R.layout.item_work_report_layout);
 
+    private int mType;
+
+    public WorkReportListAdapter(int type) {
+        super(R.layout.item_work_report_layout);
+        this.mType = type;
     }
 
     @Override
     protected void convert(BaseViewHolder helper, WorkReportListBean.ListBean item) {
         helper.setText(R.id.tv_company_name, item.getCreateCompany().getOrgName());
-        helper.setText(R.id.tv_depart_name, "部门：" + item.getCreateOrg().getOrgName());
-        helper.setText(R.id.tv_type, "类型：" + GetConstDataUtils.getWorkReportTypeList().get(item.getType()));
-        helper.setText(R.id.tv_pub_person, "发布人：" + item.getCreateUser().getAccountEntity().getRealName());
+        helper.setText(R.id.tv_depart_name, "部门            " + item.getCreateOrg().getOrgName());
+        helper.setText(R.id.tv_type, "类型            " + GetConstDataUtils.getWorkReportTypeList().get(item.getType()));
+        helper.setText(R.id.tv_pub_person, "发布人        " + item.getCreateUser().getAccountEntity().getRealName());
 //        helper.setText(R.id.tv_rev_person, "接收人：" + item.getAssigneeUser().getAccountEntity().getRealName());
-        helper.setText(R.id.tv_pub_time, "提交时间：" + item.getCreateTime());
-        if (item.getStatus() == EanfangConst.WORK_TASK_STATUS_READ) {
-            helper.setText(R.id.tv_read_ns, "已读");
+        helper.setText(R.id.tv_pub_time, "提交时间    " + item.getCreateTime());
+        if (mType == 2) {
+
+            helper.getView(R.id.tv_read_ns).setVisibility(View.VISIBLE);
+
+            if (item.getStatus() == EanfangConst.WORK_TASK_STATUS_READ) {
+                helper.setText(R.id.tv_read_ns, "已阅");
+                helper.setTextColor(R.id.tv_read_ns, Color.parseColor("#999999"));
+            } else {
+                helper.setText(R.id.tv_read_ns, "待阅");
+                helper.setTextColor(R.id.tv_read_ns, Color.parseColor("#FF4A4A"));
+            }
         } else {
-            helper.setText(R.id.tv_read_ns, "未读");
-            helper.setTextColor(R.id.tv_read_ns, Color.parseColor("#0000ff"));
+            helper.getView(R.id.tv_read_ns).setVisibility(View.INVISIBLE);
         }
         SimpleDraweeView head_pic = helper.getView(R.id.img_head);
-
-        if (item.getWorkReportDetail() != null) {
-            if (!StringUtils.isEmpty(item.getWorkReportDetail().getPictures())) {
-                String[] urls = item.getWorkReportDetail().getPictures().split(",");
-                head_pic.setImageURI(Uri.parse(BuildConfig.OSS_SERVER + urls[0]));
-            }
+        if (!StringUtils.isEmpty(item.getWorkReportDetail().getPictures())) {
+            String[] urls = item.getWorkReportDetail().getPictures().split(",");
+            head_pic.setImageURI(Uri.parse(BuildConfig.OSS_SERVER + urls[0]));
+        } else {
+            head_pic.setImageURI(Uri.parse(BuildConfig.OSS_SERVER));//默认值
         }
+
     }
 }
