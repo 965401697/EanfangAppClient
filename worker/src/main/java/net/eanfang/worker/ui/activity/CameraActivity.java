4f13c991a97e2545896c580cc138de6abbc40c2a
guanluocang <guanluocang@163.com>
2018-12-15 15:58:58 +0800

    1、相机页面修改

--- worker/src/main/java/net/eanfang/worker/ui/activity/CameraActivity.java
+++ worker/src/main/java/net/eanfang/worker/ui/activity/CameraActivity.java
@@ -400,14 +400,10 @@ public class CameraActivity extends BaseWorkerActivity implements AMapLocationLi
      * 绘制水印
      */
     private void drawBitmap(String path, Bitmap watermarkBitmap, String lAddress, String time) {
-        Bitmap textBitmap = ImageUtil.drawTextToRightBottom(this, watermarkBitmap, "地址：" + lAddress, 16, color, 5, 75);
-        textBitmap = ImageUtil.drawTextToRightBottom(this, textBitmap, "内容：" + project_content, 16, color, 5, 135);
-        textBitmap = ImageUtil.drawTextToRightBottom(this, textBitmap, "部位/区域：" + region_name, 16, color, 5, 195);
-        textBitmap = ImageUtil.drawTextToRightBottom(this, textBitmap, "名称：" + project_name, 16, color, 5, 255);
-        textBitmap = ImageUtil.drawTextToRightBottom(this, textBitmap, "类型：" + project_type, 16, color, 5, 295);
-        textBitmap = ImageUtil.drawTextToRightBottom(this, textBitmap, "创建者:" + creatUser, 16, color, 5, 335);
-        textBitmap = ImageUtil.drawTextToRightBottom(this, textBitmap, "天气：" + weather, 16, color, 5, 375);
-        textBitmap = ImageUtil.drawTextToRightBottom(this, textBitmap, "时间：" + time, 16, color, 5, 415);
+        String mContent = "时间：" + time + "\n" + "天气：" + weather + "\n" + "创建者:" +
+                creatUser + "\n" + "类型：" + project_type + "\n" + "名称：" + project_name + "\n" + "部位/区域：" + region_name + "\n" + "内容：" + project_content + "\n" +
+                "地址：" + lAddress;
+        Bitmap textBitmap = ImageUtil.drawTextToRightBottom(this, watermarkBitmap, mContent, 16, color, 5, 180);
         showTakePhotoImg.setImageBitmap(textBitmap);
         flCamera.setVisibility(View.VISIBLE);
         try {
