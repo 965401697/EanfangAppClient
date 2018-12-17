5b6c05fe8bc0369bac42a50ebeb2487754ceb3c4
ziwu <zhaoziwu@yeah.net>
2018-12-17 18:22:20 +0800

    故障记录的搜索优化

--- client/src/main/java/net/eanfang/client/ui/activity/worksapce/FaultRecordListActivity.java
+++ client/src/main/java/net/eanfang/client/ui/activity/worksapce/FaultRecordListActivity.java
@@ -118,6 +118,8 @@ public class FaultRecordListActivity extends BaseClientActivity implements Swipe
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 if (!TextUtils.isEmpty(s)) {
                     searchData(s.toString());
+                } else {
+                    refresh();
                 }
             }
 
@@ -139,6 +141,9 @@ public class FaultRecordListActivity extends BaseClientActivity implements Swipe
     }
 
     public void refresh() {
+        if (!TextUtils.isEmpty(etSearch.getText().toString())) {
+            etSearch.setText("");
+        }
         mPage = 1;//下拉永远第一页
         getData();
     }
--- worker/src/main/java/net/eanfang/worker/ui/activity/worksapce/FaultRecordListActivity.java
+++ worker/src/main/java/net/eanfang/worker/ui/activity/worksapce/FaultRecordListActivity.java
@@ -116,6 +116,8 @@ public class FaultRecordListActivity extends BaseWorkerActivity implements Swipe
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 if (!TextUtils.isEmpty(s)) {
                     searchData(s.toString());
+                } else {
+                    refresh();
                 }
             }
 
@@ -137,6 +139,9 @@ public class FaultRecordListActivity extends BaseWorkerActivity implements Swipe
     }
 
     public void refresh() {
+        if (!TextUtils.isEmpty(etSearch.getText().toString())) {
+            etSearch.setText("");
+        }
         mPage = 1;//下拉永远第一页
         getData();
     }
@@ -212,6 +217,7 @@ public class FaultRecordListActivity extends BaseWorkerActivity implements Swipe
                     }
                 });
     }
+
     private void searchData(String locationNum) {
         QueryEntry queryEntry = new QueryEntry();
         queryEntry.getLike().put("locationNumber", locationNum);
