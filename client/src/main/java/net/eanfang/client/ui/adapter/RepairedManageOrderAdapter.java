929f4fb976010273db76cefccc71c58dc957955f
liangkailun <liangkailun@cri.cn>
2019-03-29 14:39:46 +0800

    修改客户端保修订单显示逻辑

--- client/src/main/java/net/eanfang/client/ui/adapter/RepairedManageOrderAdapter.java
+++ client/src/main/java/net/eanfang/client/ui/adapter/RepairedManageOrderAdapter.java
@@ -8,47 +8,48 @@ import com.annimon.stream.Optional;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.eanfang.BuildConfig;
+import com.eanfang.application.EanfangApplication;
 import com.eanfang.util.GetConstDataUtils;
 import com.eanfang.util.GetDateUtils;
 import com.eanfang.util.NumberUtil;
 import com.eanfang.util.StringUtils;
 import com.eanfang.util.V;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.yaf.base.entity.ClientOrderOerationEntity;
 import com.yaf.base.entity.RepairOrderEntity;
 
 import net.eanfang.client.R;
 
 
 /**
- * Created by wen on 2017/5/12.
+ * @author wen on 2017/5/12.
  */
 
 public class RepairedManageOrderAdapter extends BaseQuickAdapter<RepairOrderEntity, BaseViewHolder> {
-
-
-    private final boolean[] isShowFirstBtnClient = {
-            false, false, false, false, true, true, false
-    };
-    private String[] doSomethingClient = {
-            "立即付款", "联系技师", "联系技师",
-            "联系技师", "确认完工", "评价技师", "联系技师"
-    };
-    private String[] doSomething;
-
+    private static final String ORG_NAME = "个人";
+    /**
+     * 请求地址的最短长度
+     */
+    private static final int LESS_URL_LENGTH = 10;
     public RepairedManageOrderAdapter() {
         super(R.layout.item_workspace_order_list);
-        doSomething = doSomethingClient;
 
     }
 
     @Override
     protected void convert(BaseViewHolder helper, RepairOrderEntity item) {
-        // 订单是否 已读 未读 1：新订单 0 已读
-        if (item.getNewOrder() == 1) {
-            helper.getView(R.id.tv_order_read).setVisibility(View.VISIBLE);
-        } else {
-            helper.getView(R.id.tv_order_read).setVisibility(View.GONE);
+        if (item == null) {
+            return;
         }
+        //创建订单操作类( 0:待支付，1:待回电，2:待上门，3:待完工，4:待确认，5:订单完成)
+        int status = item.getStatus();
+        boolean custom = item.getOwnerUserId() != null && item.getOwnerUserId().
+                equals(EanfangApplication.get().getUserId());
+        ClientOrderOerationEntity orderOerationEntity = new ClientOrderOerationEntity(status, custom);
+
+        // 订单是否 已读 未读 1：新订单 0 已读
+        helper.getView(R.id.tv_order_read).setVisibility(
+                item.getNewOrder() == 1 ? View.VISIBLE : View.GONE);
         String str = "";
 //        if (!StringUtils.isEmpty(item.getOriginordernum())) {
 //            str = "（挂）";
@@ -63,19 +64,11 @@ public class RepairedManageOrderAdapter extends BaseQuickAdapter<RepairOrderEnti
         if (item.getAssigneeUser() != null && item.getAssigneeUser().getAccountEntity() != null) {
             orgName += " " + Optional.ofNullable(item.getAssigneeUser().getAccountEntity().getRealName()).orElseGet(() -> "");
         }
-//        helper.setText(R.id.tv_company_name, orgName);
 
         if (item.getOwnerUser() != null && item.getOwnerUser().getAccountEntity() != null) {
             helper.setText(R.id.tv_person_name, "报修人:" + item.getOwnerUser().getAccountEntity().getRealName());
         }
 
-//        String userName = "";
-//        if (item.getOwnerUser() != null && item.getOwnerUser().getAccountEntity() != null) {
-//            userName = Optional.ofNullable(item.getOwnerUser().getAccountEntity().getRealName()).orElseGet(() -> "");
-//        }
-        /**
-         * 技师 安防公司
-         * */
         if (TextUtils.isEmpty(orgName)) {
             helper.setVisible(R.id.tv_company_name, false);
         } else {
@@ -86,7 +79,7 @@ public class RepairedManageOrderAdapter extends BaseQuickAdapter<RepairOrderEnti
         helper.setText(R.id.tv_order_id, "单号：" + item.getOrderNum() + str);
         helper.setText(R.id.tv_create_time, "下单时间：" + GetDateUtils.dateToDateTimeString(item.getCreateTime()));
 
-        if (item.getOwnerOrg() != null && item.getOwnerOrg().getBelongCompany() != null && item.getOwnerOrg().getBelongCompany().getOrgName().equals("个人")) {
+        if (item.getOwnerOrg() != null && item.getOwnerOrg().getBelongCompany() != null && ORG_NAME.equals(item.getOwnerOrg().getBelongCompany().getOrgName())) {
 
             helper.setVisible(R.id.tv_count_money, true);
             helper.setVisible(R.id.tv_total, true);
@@ -107,34 +100,21 @@ public class RepairedManageOrderAdapter extends BaseQuickAdapter<RepairOrderEnti
 
 
         helper.setText(R.id.tv_state, GetConstDataUtils.getRepairStatus().get(item.getStatus()));
-
-        helper.setVisible(R.id.tv_do_first, isShowFirstBtnClient[item.getStatus()]);
-        helper.setText(R.id.tv_do_second, doSomething[item.getStatus()]);
-
+        helper.setText(R.id.tv_do_first, orderOerationEntity.getOperationLeft());
+        helper.setText(R.id.tv_do_second, orderOerationEntity.getOperationRight());
+        helper.setVisible(R.id.tv_do_first, orderOerationEntity.isShowOperationLeft());
+        //原代码判断status为5的时候判断item.getClientEvaluateId()<=0展示否则不展示第二按钮
+        helper.setVisible(R.id.tv_do_second, orderOerationEntity.isShowOperationRight() ||
+                (custom && status == 5 &&
+                        (item.getWorkerEvaluateId() == null || item.getWorkerEvaluateId() <= 0)));
+        helper.setVisible(R.id.iv_finish, orderOerationEntity.isFinished());
+        helper.setVisible(R.id.tv_state, !orderOerationEntity.isFinished());
         // 待验收
-        if (item.getStatus() == 4) {
-            helper.setVisible(R.id.tv_finish, true);
-
-            helper.setVisible(R.id.tv_do_first, false);
-            helper.setVisible(R.id.iv_finish, false);
-        } else if (item.getStatus() == 5) {
-            helper.setText(R.id.tv_do_first, "完工报告");
-            helper.setVisible(R.id.tv_state, false);
-            helper.setVisible(R.id.iv_finish, true);
-            if (item.getWorkerEvaluateId() == null || item.getWorkerEvaluateId().longValue() <= 0) {
-                helper.setVisible(R.id.tv_do_second, true);
-            } else {
-                helper.setVisible(R.id.tv_do_second, false);
-            }
-        }else if (item.getStatus() == 6) {
-            //helper.setText(R.id.tv_do_first, "联系客户");
-            helper.setVisible(R.id.tv_do_first, false);
-            helper.setVisible(R.id.tv_do_second, false);
-        }
+        helper.setVisible(R.id.tv_finish, status == 4);
 
         //将业务类型的图片显示到列表
         String imgUrl = V.v(() -> item.getFailureEntity().getPictures().split(",")[0]);
-        if (!StringUtils.isEmpty(imgUrl) && imgUrl.length() > 10) {
+        if (!StringUtils.isEmpty(imgUrl) && imgUrl.length() > LESS_URL_LENGTH) {
             ((SimpleDraweeView) helper.getView(R.id.iv_upload)).setImageURI(Uri.parse(BuildConfig.OSS_SERVER + imgUrl));
         } else {
             ((SimpleDraweeView) helper.getView(R.id.iv_upload)).setImageURI(Uri.parse(BuildConfig.OSS_SERVER + ""));
