fa2fd8a0466392727b2110b0ec51371ce9469076
guanluocang <guanluocang@163.com>
2019-05-13 16:26:05 +0800

    1、安防圈个人中心关注 列表未发生变化 进行修改。

--- worker/src/main/java/net/eanfang/worker/ui/activity/worksapce/security/SecurityDetailActivity.java
+++ worker/src/main/java/net/eanfang/worker/ui/activity/worksapce/security/SecurityDetailActivity.java
@@ -641,6 +641,7 @@ public class SecurityDetailActivity extends BaseActivity implements Parser.OnPar
         if (resultCode == Activity.RESULT_OK && data != null) {
             if (mIsPubUid) {
                 isFoucus = data.getBooleanExtra(UserHomeActivity.RESULT_FOLLOW_STATE, true);
+                isFoucsEdit = isFoucus;
                 tvIsFocus.setText(isFoucus ? "取消关注" : "关注");
             }
         }
