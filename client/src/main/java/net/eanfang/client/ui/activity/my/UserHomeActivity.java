e0749c63beaea02e73e31c03e3dc3b5e743c672a
liangkailun <liangkailun@cri.cn>
2019-06-05 16:55:55 +0800

    聊天界面点击查看详情返回后刷新用户头像

--- client/src/main/java/net/eanfang/client/ui/activity/my/UserHomeActivity.java
+++ client/src/main/java/net/eanfang/client/ui/activity/my/UserHomeActivity.java
@@ -283,6 +283,7 @@ public class UserHomeActivity extends BaseClientActivity {
                     if (accountBean != null) {
                         mUserInfo = new UserInfo(accountBean.getAccId(),
                                 accountBean.getNickName(), Uri.parse(BuildConfig.OSS_SERVER + accountBean.getAvatar()));
+                        RongIM.getInstance().refreshUserInfoCache(mUserInfo);
                         mImgUserHeader.setImageURI(Uri.parse(BuildConfig.OSS_SERVER + accountBean.getAvatar()));
                         mTvNickname.setText(accountBean.getNickName());
                         mTvNickname.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
