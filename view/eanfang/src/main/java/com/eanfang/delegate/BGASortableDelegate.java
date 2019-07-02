79561bdecf6a2f45a8edee84263be1a4c177f560
dongjinjuan <dongjinjuan@mallguang.com>
2019-07-02 13:22:08 +0800

    还原原来的图片选择部分代码，以保证项目正常运行

--- view/eanfang/src/main/java/com/eanfang/delegate/BGASortableDelegate.java
+++ view/eanfang/src/main/java/com/eanfang/delegate/BGASortableDelegate.java
@@ -6,7 +6,7 @@ import android.view.View;
 import com.eanfang.http.EanfangCallback;
 import com.eanfang.ui.base.BaseActivity;
 import com.eanfang.ui.base.BaseActivityWithTakePhoto;
-import com.eanfang.base.kit.rx.RxPerm;
+import com.eanfang.util.PermissionUtils;
 import com.photopicker.com.activity.BGAPhotoPickerActivity;
 import com.photopicker.com.activity.BGAPhotoPickerPreviewActivity;
 import com.photopicker.com.widget.BGASortableNinePhotoLayout;
@@ -60,11 +60,11 @@ public class BGASortableDelegate implements
         // 拍照后照片的存放目录，改成你自己拍照后要存放照片的目录。如果不传递该参数的话就没有拍照功能
         File takePhotoDir = new File(Environment.getExternalStorageDirectory(), "EanfangPhotoData");
         if (activity != null) {
-            RxPerm.get(activity).cameraPerm((isSuccess)->{
+            PermissionUtils.get(activity).getCameraPermission(() -> {
                 activity.startActivityForResult(BGAPhotoPickerActivity.newIntent(activity, takePhotoDir, sortableNinePhotoLayout.getMaxItemCount() - sortableNinePhotoLayout.getItemCount(), null, false), codes.get(0));
             });
         } else {
-            RxPerm.get(activity).cameraPerm((isSuccess)->{
+            PermissionUtils.get(activityWithTakePhoto).getCameraPermission(() -> {
                 activityWithTakePhoto.startActivityForResult(BGAPhotoPickerActivity.newIntent(activityWithTakePhoto, takePhotoDir, sortableNinePhotoLayout.getMaxItemCount() - sortableNinePhotoLayout.getItemCount(), null, false), codes.get(0));
             });
         }
@@ -78,11 +78,11 @@ public class BGASortableDelegate implements
     @Override
     public final void onClickNinePhotoItem(BGASortableNinePhotoLayout sortableNinePhotoLayout, View view, int position, String model, ArrayList<String> models) {
         if (activity != null) {
-            RxPerm.get(activity).cameraPerm((isSuccess)->{
+            PermissionUtils.get(activity).getCameraPermission(() -> {
                 activity.startActivityForResult(BGAPhotoPickerPreviewActivity.newIntent(activity, sortableNinePhotoLayout.getItemCount(), models, models, position, false), codes.get(1));
             });
         } else {
-            RxPerm.get(activity).cameraPerm((isSuccess)->{
+            PermissionUtils.get(activityWithTakePhoto).getCameraPermission(() -> {
                 activityWithTakePhoto.startActivityForResult(BGAPhotoPickerPreviewActivity.newIntent(activityWithTakePhoto, sortableNinePhotoLayout.getItemCount(), models, models, position, false), codes.get(1));
             });
         }
